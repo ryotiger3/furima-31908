1208e012e73b5ad59308037e794ae596bbdc6be5
ryotiger3 <rothsryo319@gmail.com>
2021-03-01 17:13:45 +0900

    テストコードの追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,6 +43,13 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
 
+      it '重複したemailが存在する場合登録できないこと' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+
       it 'passwordが5文字以下であれば登録できないこと' do
         @user.password = '12345'
         @user.password_confirmation = '12345'
