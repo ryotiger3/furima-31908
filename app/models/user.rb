510b431a6a00789a2b3db79c265a9dd6faafc33f
ryotiger3 <rothsryo319@gmail.com>
2021-03-01 14:40:02 +0900

    バリデーションとテストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :products
   has_many :purchase_histories
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,           presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password,         format: { with: VALID_PASSWORD_REGEX }
+
   validates :nickname,         presence: true
   validates :family_name,      presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name,       presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {"test"}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
+    password = '1a' + Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
     family_name {"山田"}
