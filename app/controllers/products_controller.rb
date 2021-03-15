3b59f78a8a651e248527a89285b0a9c169144fa2
ryotiger3 <rothsryo319@gmail.com>
2021-03-15 17:29:29 +0900

    商品削除機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,12 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to root_path
+  end
+
     private
     def product_params
       params.require(:product).permit(:name, :price, :description, :category_id, :condition_id, :shipping_charged_id, :prefecture_id, :shipping_days_id, :image).merge(user_id: current_user.id)
