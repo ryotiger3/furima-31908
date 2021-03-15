20fbb2e3cb1fb38baf40aa6ec8477a057ba3ac73
ryotiger3 <rothsryo319@gmail.com>
2021-03-15 16:23:27 +0900

    商品詳細表示機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,10 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
     private
     def product_params
       params.require(:product).permit(:name, :price, :description, :category_id, :condition_id, :shipping_charged_id, :prefecture_id, :shipping_days_id, :image).merge(user_id: current_user.id)
